cb327966d71d9d59a4c6d197500336fa0e319e86
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-01 20:31:38 -0400

    fix: fixed 404 error when requesting endpoints

--- Program.cs
+++ Program.cs
@@ -6,51 +6,79 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at
 // https://aka.ms/aspnetcore/swashbuckle
+
+// added controllers
+builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// configure AppDbContext 
+// configure AppDbContext
 builder.Services.AddDbContext<AppDbContext>(
     // using inMemory / caching during development for quick interaction and testing
     // connect db for production to save state
-    option => option.UseInMemoryDatabase("InMem"));
+    option =>
+    option.UseInMemoryDatabase("InMem")
+);
 
 // map concrete type from interface to PlatformRepo class; dependency injection
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
+// configure AutoMapper for dependency injection
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 PrepDb.PrepPopulation(app);
 
 app.UseHttpsRedirection();
 
-var summaries = new[] {
-  "Freezing", "Bracing", "Chilly", "Cool",       "Mild",
-  "Warm",     "Balmy",   "Hot",    "Sweltering", "Scorching",
+// map controllers to endpoints
+app.MapControllers();
+
+var summaries = new[]
+{
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching",
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-  var forecast =
-      Enumerable.Range(1, 5)
-          .Select(index => new WeatherForecast(
-                      DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                      Random.Shared.Next(-20, 55),
-                      summaries[Random.Shared.Next(summaries.Length)]))
-          .ToArray();
-  return forecast;
-}).WithName("GetWeatherForecast").WithOpenApi();
+// mock endpoint for testing from dotnet template
+app.MapGet(
+        "/weatherforecast",
+        () =>
+        {
+            var forecast = Enumerable
+                .Range(1, 5)
+                .Select(index => new WeatherForecast(
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+            return forecast;
+        }
+    )
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
 
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
-  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
