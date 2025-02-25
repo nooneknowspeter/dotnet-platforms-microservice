bdd2ba8ad707da28d5dcb3e8e98f9cc485608fdd
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-25 15:30:24 -0400

    style: csharpier

--- Data/IPlatformRepo.cs
+++ Data/IPlatformRepo.cs
@@ -2,16 +2,16 @@ using PlatformService.Models;
 
 namespace PlatformService.Data
 {
-  // interface for facade design for interact with PlatformRepo
-  public interface IPlatformRepo
-  {
-    // save changes when changing DbContext
-    bool SaveChanges();
+    // interface for facade design for interact with PlatformRepo
+    public interface IPlatformRepo
+    {
+        // save changes when changing DbContext
+        bool SaveChanges();
 
-    // enumerable to for users to retrieve all platforms, 
-    // and other information on platforms; facade interaction
-    IEnumerable<Platform> GetAllPlatforms();
-    Platform GetPlatformById(int id);
-    void CreatePlatform(Platform plat);
-  }
+        // enumerable for users to retrieve all platforms, 
+        // and other information on platforms; facade interaction
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform plat);
+    }
 }
