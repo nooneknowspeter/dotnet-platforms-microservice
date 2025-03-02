7739b64813880a29ad83e0223f2e69ffa2caaa2a
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-03-02 00:12:58 -0400

    feat: mapped gRPC to stored Platform object

--- Profiles/PlatformsProfile.cs
+++ Profiles/PlatformsProfile.cs
@@ -13,6 +13,11 @@ namespace PlatformService.Profiles
       CreateMap<Platform, PlatformReadDto>();
       CreateMap<PlatformCreateDto, Platform>();
       CreateMap<PlatformReadDto, PlatformPublishedDto>();
+      // maps Platform object to gRPC Object
+      CreateMap<Platform, GrpcPlatformModel>()
+        .ForMember(destination => destination.PlatformId, option => option.MapFrom(source => source.Id))
+        .ForMember(destination => destination.Name, option => option.MapFrom(source => source.Name))
+        .ForMember(destination => destination.Publisher, option => option.MapFrom(source => source.Publisher));
     }
   }
 }
